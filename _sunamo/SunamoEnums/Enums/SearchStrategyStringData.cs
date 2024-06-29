4629f83b420c8c353f72029f339c58f9a2605b33
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:51 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoEnums/Enums/SearchStrategyStringData.cs
@@ -0,0 +1,23 @@
+namespace SunamoStringData;
+
+
+/// <summary>
+/// FixedSpace - Contains
+/// AnySpaces -
+/// ExactlyName - Is exactly the same
+/// </summary>
+internal enum SearchStrategyStringData
+{
+    /// <summary>
+    /// Contains
+    /// </summary>
+    FixedSpace,
+    /// <summary>
+    /// split input by spaces and A1 must contains all parts
+    /// </summary>
+    AnySpaces,
+    /// <summary>
+    /// Is exactly the same
+    /// </summary>
+    ExactlyName
+}
